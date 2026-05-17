ed5f49127b7ff98bd86312ced7534c81cb2f4a0b
debugmi530-del <debugmi530@gmail.com>
2026-05-17 20:31:06 +0300

    update app/src/main/java/com/diepclone/app/MainActivity.java

--- app/src/main/java/com/diepclone/app/MainActivity.java
+++ app/src/main/java/com/diepclone/app/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,18 +15,22 @@ public class MainActivity extends Activity {
 
     private WebView webView;
 
+    class AndroidBridge {
+        @JavascriptInterface
+        public void exitApp() {
+            finishAffinity();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Fullscreen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
-
-        // Hide system UI
         getWindow().getDecorView().setSystemUiVisibility(
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -34,6 +39,8 @@ public class MainActivity extends Activity {
         );
 
         webView = new WebView(this);
+        webView.addJavascriptInterface(new AndroidBridge(), "Android");
+
         WebSettings s = webView.getSettings();
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
@@ -53,22 +60,26 @@ public class MainActivity extends Activity {
 
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        webView.evaluateJavascript(
+            "if(typeof window.__gamePause==='function'){window.__gamePause();}",
+            null
+        );
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         webView.onResume();
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         webView.onPause();
+        webView.evaluateJavascript(
+            "if(typeof window.__gamePause==='function'&&window.__gamePauseActive!==true){window.__gamePause();}",
+            null
+        );
     }
 }
